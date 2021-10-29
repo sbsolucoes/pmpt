229627bd9ca5fd244f14a3c21b05999da17bd2e9
sbsolucoes <85140013+sbsolucoes@users.noreply.github.com>
2021-10-28 22:33:31 -0300

    Update Licitacao.php
    correção da tabela licitação

--- app/Models/Licitacao.php
+++ app/Models/Licitacao.php
@@ -12,7 +12,7 @@ class Licitacao extends Model
         /* como vc n esta trabalhando com o padrão do laravel, sempre informa com o nome da chame primaria.
     *
     * dentro da variavel $fillable, n precisa informar o id da tabela, só fazer como eu fiz*/
-    protected $table = 'licitacoes';
+    protected $table = 'licitacao';
     protected $primaryKey = 'idLicitacao';
     protected $fillable = ['tipoLicitacao', 
                             'numeroLicitacao',
