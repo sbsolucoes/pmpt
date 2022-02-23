af44af768718b08fce5cb4f7e94910e01d919bda
silvio <silviojosebatista@gmail.com>
2022-02-23 00:08:35 -0300

    correcao alto constraste

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -75,9 +75,9 @@
             <div class="form-check form-switch m-">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-half" viewBox="0 0 20 16">
                     <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-                  </svg>
-                <input class="form-check-input" type="checkbox" id="switch" name="theme">
-                <label class="form-check-label" for="switch">Alto Contraste</label>
+                  </svg> 
+                <input class="form-check-input" type="checkbox" id="switch" name="theme">                
+                <label class="form-check-label" for="switch">Alto Contraste</label>                
             </div>
 
             <div class="btn-container m-1" text-primary>
