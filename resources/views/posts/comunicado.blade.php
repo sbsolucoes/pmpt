b74b7f65494a09b8200e9d77314e18dc899cf640
silvio <silviojosebatista@gmail.com>
2022-05-10 23:30:29 -0300

    comunicados heith

--- resources/views/posts/comunicado.blade.php
+++ resources/views/posts/comunicado.blade.php
@@ -3,7 +3,7 @@
 
 <div class="container-fluid">
     <div class="row float-left">
-        <div class="col-md-12 mh-100" style="height: 600px;">
+        <div class="col-md-12 mh-100" >
 
             <h3 class="title text-dark text-left my-3">{{$comunicados->com_titulo}}</h3>
             <hr class="mt-2 mb-2">
