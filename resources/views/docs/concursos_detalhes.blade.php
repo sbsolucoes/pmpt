4d019f67181ff694638dfee3de2718f1625a5dc2
silvio <silviojosebatista@gmail.com>
2022-06-06 20:22:27 -0300

    declaracao anual de bens e down concursos-3

--- resources/views/docs/concursos_detalhes.blade.php
+++ resources/views/docs/concursos_detalhes.blade.php
@@ -33,7 +33,7 @@
                                         <td>{{ date('d/m/Y', strtotime($down['dtPub'])) }}</td>
                                         <td>
                                             <a href="{{$down->doc}}" download="{{$down->doc}}"  class="btn btn-primary" target="_blank">Abrir</a>
-                                            <!--<a href="{{ route('downloadfile',  $down->doc) }}" class="btn btn-primary"  target="_blank">Baixar</a>-->
+                                            
                                         </td>
                                     </tr>
                                 @endforeach
