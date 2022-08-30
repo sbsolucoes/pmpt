c4132ad171e589413abb08dcfa40cf0ec957a659
silvio <silviojosebatista@gmail.com>
2022-08-29 21:01:28 -0300

    correção pesquisa atos

--- resources/views/docs/atos.blade.php
+++ resources/views/docs/atos.blade.php
@@ -17,48 +17,17 @@
                         <h2 class="title">Pesquisar Atos</h2>
                         <br>
 
-                        <!--
+                        
                         <form  method="post"  >
                             @csrf
                             <input type="text" name="nome" placeholder="Digite a pesquisa" />
                             <input type="submit" value="Buscar" />
 
                         </form>
-                    -->
+                    
 
-                    <!--Inicio pesquisa detalhes -->
-                <form class="form-horizontal" method="post" action="{{ route('docs.atos') }}">
-                    <div class="form4">
-                        @csrf
-                        <div class="form-group form-group-sm">
-                            <label class="control-label col-sm-3" for="nome">Documento:</label>
-                            <div class="col-sm-8">
-                                <input class="form-control" type="text" name="nome" value="{{ old('nome') }}">
-                            </div>
-                        </div>
-            
-                        <div class="form-group form-group-sm">
-                            <label class="control-label col-sm-3" for="data1">Período:</label>
-                          <div class="col-sm-3">
-                              <input class="form-control" type="date" name="data1" value="{{ old('data1') }}">
-                          </div>
-                          <label class="control-label col-sm-1" for="data2">A</label>
-                          <div class="col-sm-3">
-                              <input class="form-control" type="date" name="data2" value="{{ old('data2') }}">
-                          </div>
-                        </div>            
-            
-                        <div class="form-central" style="margin-top: 35px;">
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">
-                                <i style="font-size: 20px;" class="glyphicon glyphicon-search" aria-hidden="true"></i> Pesquisar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                    
 
-                    <!--Fim Pesquisa Detalhes-->
                     </div>
                     <table class="table table-striped">
                         <thead>
