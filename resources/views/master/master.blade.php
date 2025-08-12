4a417589f6530c00786c86d539cf969f26bb82c9
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2025-08-11 22:04:58 -0300

    feat: poup de anuncios

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -35,12 +35,20 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<!--Alerts-->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!--<script src="{{ asset('js/scripts.js') }}"></script>-->
 <!--ZOOM-->
 <script type="text/javascript">
     window.onload = function() {
+
+        Swal.fire({
+            imageUrl: "images/pop-up.jpg",
+            imageAlt: "Cerimônia de posse",
+            confirmButtonText: "Fechar",
+            confirmButtonColor: "darkred",
+        });
+
         var elementBody = document.querySelector('body');
         var elementBtnIncreaseFont = document.getElementById('increase-font');
         var elementBtnDecreaseFont = document.getElementById('decrease-font');
